da5509033fccd75928449704faa74399470c9e56
Emanuele Pucciarelli <ep@corp.it>
2011-02-28 00:47:10 +0100

    Updated copyright notices and licensing info

--- faxcelerate/fax/filterspec.py
+++ faxcelerate/fax/filterspec.py
@@ -1,3 +1,25 @@
+# Copyright 2007-2011 C.O.R.P. s.n.c.
+#
+# This file is part of Faxcelerate.
+# Faxcelerate is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU Affero General Public
+# License, as published by the Free Software Foundation.
+#
+# Faxcelerate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with Faxcelerate.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+__author__			= "Emanuele Pucciarelli"
+__organization__	= "C.O.R.P. s.n.c"
+__copyright__		= "Copyright 2007-2011, C.O.R.P. s.n.c"
+__license__ 		= "GNU Affero GPL v. 3.0"
+__contact__			= "faxcelerate@corp.it"
+
 from django.contrib.admin.filterspecs \
     import FilterSpec, RelatedFilterSpec, DateFieldFilterSpec
 from django.utils.translation import ugettext_lazy as _
