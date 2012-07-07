99ca93d94d6dac587795278394665ad7462372de
Emanuele Pucciarelli <ep@corp.it>
2012-07-07 11:10:24 +0200

    More setup process fixes

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+install_requires = []
+dependency_links = []
+for l in open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines():
+    l = l.strip()
+    if '://' in l:
+        dependency_links += [l]
+    else:
+        install_requires += [l]
+
 setup(
     name = "faxcelerate",
     version = "0.1.1",
@@ -24,5 +33,6 @@ setup(
         "Topic :: Communications :: Fax",
         "License :: OSI Approved :: GNU Affero General Public License v3",
     ],
-    install_requires=open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines(),
+    install_requires=install_requires,
+    dependency_links=dependency_links,
 )
